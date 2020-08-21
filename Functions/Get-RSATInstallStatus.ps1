4169415b0d60d64f7e80567c1d967e4c8e123202
Jarrod Rother <Jarrod.Rother@Knowles.com>
2020-08-21 14:03:34 -0400

    Fixed RSAT Files

--- Functions/Get-RSATInstallStatus.ps1
+++ Functions/Get-RSATInstallStatus.ps1
@@ -24,30 +24,3 @@ function Get-RSATInstallStatus {
     } #END
 
 } #FUNCTION
-
-function Invoke-RSATInstallation {
-    <#
-        .SYNOPSIS
-            Installs all RSAT Tools on Current PC
-        .DESCRIPTION
-            Queries all RSAT Tools available and Installs them. Requires internet Access
-        .EXAMPLE
-            Invoke-RSATInstallation
-    #>
-    [CmdletBinding()]
-    Param(
-        
-    ) 
-    BEGIN { 
-
-    } #BEGIN
-
-    PROCESS {
-        Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
-    } #PROCESS
-
-    END { 
-
-    } #END
-
-} #FUNCTION
