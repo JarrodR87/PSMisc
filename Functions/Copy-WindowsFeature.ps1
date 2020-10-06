6554521a5f733c97c9d157734a357fce22cbcb26
Jarrod Rother <Jarrod.Rother@Knowles.com>
2020-10-06 16:33:06 -0400

    Added Windows Feature and Local BitLocker Volume

---
+++ Functions/Copy-WindowsFeature.ps1
@@ -0,0 +1,31 @@
+function Copy-WindowsFeature {
+    <#
+        .SYNOPSIS
+            Copies Installed Windows Features from one Computer to another
+        .DESCRIPTION
+            Collects Installed Windows Features from one Computer and pipes that into a command to install them on another Computer
+        .PARAMETER SourceComputerName
+            Computer to collect list of Installed Windows Features From
+        .PARAMETER DestinationComputerName
+            Computer to Install Features
+        .EXAMPLE
+            Copy-WindowsFeature -SourceComputerName 'TESTPC1' -DestinationComputerName 'TestPC2'
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]$SourceComputerName,
+        [Parameter(Mandatory = $true)]$DestinationComputerName
+    ) 
+    BEGIN { 
+        $WindowsFeatures = Invoke-Command -ComputerName $SourceComputerName -ScriptBlock { (Get-WindowsFeature | where-object { $_.Installed -like 'TRUE' }) }
+    } #BEGIN
+
+    PROCESS {
+        Install-WindowsFeature $WindowsFeatures
+    } #PROCESS
+
+    END { 
+
+    } #END
+
+} #FUNCTION
---
+++ Functions/Get-LocalBitLockerReoveryKeys.ps1
@@ -0,0 +1,34 @@
+﻿function Get-LocalBitLockerReoveryKeys {
+    <#
+        .SYNOPSIS
+            Collects Local BitLocker Reovery Keys
+        .DESCRIPTION
+            Collects Recovery Keys for Local BitLocker Encrypted Drives
+        .EXAMPLE
+            Get-LocalBitLockerReoveryKeys
+    #>
+    [CmdletBinding()]
+    Param(
+        
+    ) 
+    BEGIN { 
+        $BitLockerVolumes = Get-BitLockerVolume
+        $BitLockerReoveryKeys = @()
+    } #BEGIN
+
+    PROCESS {
+        foreach ($BitLockerVolume in $BitLockerVolumes) {
+            $Row = New-Object PSObject
+            $Row | Add-Member -MemberType noteproperty -Name "MountPoint" -Value $BitLockerVolume.MountPoint
+            $Row | Add-Member -MemberType noteproperty -Name "RecoveryKey" -Value ($BitLockerVolume.KeyProtector | where-object { $_.KeyProtectorType -like 'RecoveryPassword' }).RecoveryPassword
+
+            $BitLockerReoveryKeys += $Row
+            $BitLockerVolume = $NULL
+        }
+    } #PROCESS
+
+    END { 
+        $BitLockerReoveryKeys
+    } #END
+
+} #FUNCTION
